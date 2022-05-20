fd912c215b5aec68493737a4cd5de72f9086af3b
PLMC581 <shvan581@gmail.com>
2022-05-20 18:18:09 -0400

    Added Basic Player Jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 15f;
-    
+    [SerializeField] private float _jumpVelocity = 15f;
+
     private Rigidbody2D _rigidBody;
 
     private void Awake()
@@ -12,10 +13,20 @@ public class PlayerController : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpVelocity);
+        }
+    }
+
     private void FixedUpdate()
     {
         var horizontal = Input.GetAxis("Horizontal") * _speed;
 
         _rigidBody.velocity = new Vector2(horizontal, _rigidBody.velocity.y);
+
+        
     }
 }
