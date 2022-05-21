84f27c154d65672f68d8e71017deff23ddbb0231
PLMC581 <shvan581@gmail.com>
2022-05-20 22:41:43 -0400

    Added Double Jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 15f;
     [SerializeField] private float _jumpVelocity = 15f;
+    [SerializeField] private int _maxJumps = 2;
+    [SerializeField] private Transform _feet;
+    [SerializeField] private float _groundingRadius = .1f;
+    [SerializeField] private LayerMask _layer;
 
+    private int _currentJumps;
     private Rigidbody2D _rigidBody;
 
     private void Awake()
@@ -13,11 +18,29 @@ public class PlayerController : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        _currentJumps = _maxJumps;
+    }
+
     private void Update()
     {
+        var hit = Physics2D.OverlapCircle(_feet.position, _groundingRadius, _layer);
+        bool isGrounded = hit != null;
+        
         if (Input.GetButtonDown("Fire1"))
         {
-            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpVelocity);
+            if (_currentJumps > 0)
+            {
+                _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpVelocity);
+            }
+
+            _currentJumps--;
+        }
+
+        if (isGrounded)
+        {
+            _currentJumps = _maxJumps;
         }
     }
 
